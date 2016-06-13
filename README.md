a16299c18dc764933307fb170861bb158c01ec0c
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-13 15:52:16 -0500

    Images import for 3 feeds, and cards are styled

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # XML Parsing (Prototype)
 
 Just a quick exercise in parsing XML in javascript--part of planning for my NSS capstone project.
+
+Please excuse the messy code. It's all in the name of science.
--- main.js
+++ main.js
@@ -17,13 +17,15 @@ testDataWired.addEventListener("load", function(event) {
     let title = item.getElementsByTagName("title");
     let author = item.getElementsByTagName("creator");
     let link = item.getElementsByTagName("link");
-    console.log("link: ", link);
-    let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent);
+    let description = item.getElementsByTagName("description");
+    let innerDesc = description[0].textContent.match(/src="[^\"]*"/gim);
+    let image = innerDesc[0].replace(/src=|"/gim, "");
+    let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent, image);
     itemsWired.push(newItem);
   });
   let buildHTML = `<h3>Wired</h3>`;
   for (var i = 0; i < 10; i++) {
-    buildHTML += `<p><a href="${itemsWired[i].link}">${itemsWired[i].title}</a></p><p>${itemsWired[i].author}</p>`
+    buildHTML += `<div class="content-card"><p><img src="${itemsWired[i].image}" /></p><div class="card-text"<p><a href="${itemsWired[i].link}">${itemsWired[i].title}</a></p><p> - ${itemsWired[i].author}</p></div></div>`
   }
   outputWired.innerHTML = buildHTML;
 })
@@ -39,39 +41,53 @@ testDataPitchfork.addEventListener("load", function(event) {
     let title = item.getElementsByTagName("title");
     let author = item.getElementsByTagName("creator");
     let link = item.getElementsByTagName("link");
-    let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent);
+    let description = item.getElementsByTagName("description");
+    let innerDesc = description[0].textContent.match(/src="[^\"]*"/gim);
+    let image = innerDesc[0].replace(/src=|"/gim, "");
+    let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent, image);
     itemsPitchfork.push(newItem);
   });
   let buildHTML = `<h3>Pitchfork</h3>`;
   for (var i = 0; i < 10; i++) {
-    buildHTML += `<p><a href="${itemsPitchfork[i].link}">${itemsPitchfork[i].title}</a></p><p>${itemsPitchfork[i].author}</p>`
+    buildHTML += `<div class="content-card"><p><img src="${itemsPitchfork[i].image}" /></p><div class="card-text"<p><a href="${itemsPitchfork[i].link}">${itemsPitchfork[i].title}</a></p><p> - ${itemsPitchfork[i].author}</p></div></div>`
   }
   outputPitchfork.innerHTML = buildHTML;
 })
 
 const testDataNYTimes = new XMLHttpRequest();
-testDataNYTimes.open("GET", "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
+testDataNYTimes.open("GET", "http://rss.nytimes.com/services/xml/rss/nyt/Technology.xml");
 testDataNYTimes.send();
 testDataNYTimes.addEventListener("load", function(event) {
   const testXML = this.responseXML;
+  console.log("testXML: ", testXML);
   const allItemData = testXML.getElementsByTagName("item");
   const allItems = Array.from(allItemData);
   allItems.forEach(function(item) {
     let title = item.getElementsByTagName("title");
     let author = item.getElementsByTagName("creator");
     let link = item.getElementsByTagName("link");
-    let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent);
-    itemsNYTimes.push(newItem);
+    let image = "";
+    let media = item.getElementsByTagName("content");
+    console.log("media: ", media);
+    if (media.length !== 0) {
+      let mediaParse = media[0].outerHTML.match(/url="([^\"]*)"/g);
+      console.log("mediaParse: ", mediaParse);
+      let image = mediaParse[0].replace(/url=|"/g, "");
+      console.log("image: ", image);
+      let newItem = new NewItem(title[0].textContent, author[0].textContent, link[0].textContent, image);
+      itemsNYTimes.push(newItem);
+    }
   });
-  let buildHTML = `<h3>NYTimes</h3>`;
+  let buildHTML = `<h3>NYTimes (Tech)</h3>`;
   for (var i = 0; i < 10; i++) {
-    buildHTML += `<p><a href="${itemsNYTimes[i].link}">${itemsNYTimes[i].title}</a></p><p>${itemsNYTimes[i].author}</p>`
+    buildHTML += `<div class="content-card"><p><img src="${itemsNYTimes[i].image}" /></p><div class="card-text"<p><a href="${itemsNYTimes[i].link}">${itemsNYTimes[i].title}</a></p><p> - ${itemsNYTimes[i].author}</p></div></div>`
   }
   outputNYTimes.innerHTML = buildHTML;
 })
 
-function NewItem(title, author, link) {
+function NewItem(title, author, link, image) {
   this.title = title;
   this.author = author;
   this.link = link;
+  this.image = image;
 }
