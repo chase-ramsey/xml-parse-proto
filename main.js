b86764da4392a8a15ed0cf6a8f93fe0d110c8652
Chase Ramsey <ramsey.chase@gmail.com>
2016-06-13 13:50:25 -0500

    Installed bootstrap; added two feeds
    RSS feeds from Pitchfork and the NYTimes home page are called in, and all 3 feeds display in bootstrap columns

--- main.js
+++ main.js
@@ -1,30 +1,69 @@
-let items = [];
+let itemsWired = [];
+let itemsPitchfork = [];
+let itemsNYTimes = [];
 
-const output = document.getElementById("output");
+const outputWired = document.getElementById("output1");
+const outputPitchfork = document.getElementById("output2");
+const outputNYTimes = document.getElementById("output3");
 
-const testData = new XMLHttpRequest();
-testData.open("GET", "http://www.wired.com/feed/");
-testData.send();
-testData.addEventListener("load", function(event) {
-  console.log("event: ", event);
+const testDataWired = new XMLHttpRequest();
+testDataWired.open("GET", "http://www.wired.com/feed/");
+testDataWired.send();
+testDataWired.addEventListener("load", function(event) {
   const testXML = this.responseXML;
   const allItemData = testXML.getElementsByTagName("item");
-  console.log("testXML: ", testXML);
-  console.log("allItemData: ", allItemData);
   const allItems = Array.from(allItemData);
-  console.log("allItems: ", allItems);
   allItems.forEach(function(item) {
     let title = item.getElementsByTagName("title");
     let author = item.getElementsByTagName("creator");
     let newItem = new NewItem(title[0].textContent, author[0].textContent);
-    items.push(newItem);
+    itemsWired.push(newItem);
   });
-  console.log("items after load: ", items);
-  let buildHTML = '';
-  items.forEach(function(item) {
+  let buildHTML = `<h3>Wired</h3>`;
+  itemsWired.forEach(function(item) {
     buildHTML += `<p>${item.title}</p><p> - ${item.author}</p>`
   })
-  output.innerHTML = buildHTML;
+  outputWired.innerHTML = buildHTML;
+})
+
+const testDataPitchfork = new XMLHttpRequest();
+testDataPitchfork.open("GET", "http://pitchfork.com/rss/news/");
+testDataPitchfork.send();
+testDataPitchfork.addEventListener("load", function(event) {
+  const testXML = this.responseXML;
+  const allItemData = testXML.getElementsByTagName("item");
+  const allItems = Array.from(allItemData);
+  allItems.forEach(function(item) {
+    let title = item.getElementsByTagName("title");
+    let author = item.getElementsByTagName("creator");
+    let newItem = new NewItem(title[0].textContent, author[0].textContent);
+    itemsPitchfork.push(newItem);
+  });
+  let buildHTML = `<h3>Pitchfork</h3>`;
+  itemsPitchfork.forEach(function(item) {
+    buildHTML += `<p>${item.title}</p><p> - ${item.author}</p>`
+  })
+  outputPitchfork.innerHTML = buildHTML;
+})
+
+const testDataNYTimes = new XMLHttpRequest();
+testDataNYTimes.open("GET", "http://rss.nytimes.com/services/xml/rss/nyt/HomePage.xml");
+testDataNYTimes.send();
+testDataNYTimes.addEventListener("load", function(event) {
+  const testXML = this.responseXML;
+  const allItemData = testXML.getElementsByTagName("item");
+  const allItems = Array.from(allItemData);
+  allItems.forEach(function(item) {
+    let title = item.getElementsByTagName("title");
+    let author = item.getElementsByTagName("creator");
+    let newItem = new NewItem(title[0].textContent, author[0].textContent);
+    itemsNYTimes.push(newItem);
+  });
+  let buildHTML = `<h3>NYTimes</h3>`;
+  itemsNYTimes.forEach(function(item) {
+    buildHTML += `<p>${item.title}</p><p> - ${item.author}</p>`
+  })
+  outputNYTimes.innerHTML = buildHTML;
 })
 
 function NewItem(title, author) {
